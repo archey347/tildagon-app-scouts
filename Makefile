ec54a9041353565c0ef3e67cc23fc0efe95ae366
archey347 <archey.barrell@outlook.com>
2024-06-26 16:33:25 +0100

    Many Things spinny leds smooth colour transitions re-enable pattern

--- Makefile
+++ Makefile
@@ -8,6 +8,16 @@ update:
 	/root/.local/bin/mpremote a0 cp ./metadata.json :/apps/scouts/
 	/root/.local/bin/mpremote a0 cp ./assets/* :/apps/scouts/assets/
 
+uninstall:
+	-/root/.local/bin/mpremote a0 rm /apps/scouts/app.py
+	-/root/.local/bin/mpremote a0 rm /apps/scouts/metadata.json
+	-/root/.local/bin/mpremote a0 rm /apps/scouts/assets/se.jpg
+	-/root/.local/bin/mpremote a0 rm /apps/scouts/assets/fleur.jpg
+	-/root/.local/bin/mpremote a0 rm /apps/scouts/assets/cake.jpg
+
+	-/root/.local/bin/mpremote a0 rmdir /apps/scouts/assets
+	-/root/.local/bin/mpremote a0 rmdir /apps/scouts
+
 shell:
 	/root/.local/bin/mpremote a0
 
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import app as app
 import sys
 import os
 import time
+import math
 
 from app_components import clear_background
 from events.input import Buttons, BUTTON_TYPES
@@ -25,6 +26,16 @@ SCOUTS_FOREST_GREEN = (32, 91, 65)
 SE_DARK_GREEN = (120, 146, 75)
 SE_LIGHT_GREEN = (183, 213, 67)
 
+WAIT = 10
+
+def getT():
+    return time.time_ns() / (1e9 * WAIT)
+
+intensity = 1
+colour = SCOUTS_PURPLE
+last_colour = SCOUTS_PURPLE
+last_change = getT()
+
 if sys.implementation.name == "micropython":
     apps = os.listdir("/apps")
     path = ""
@@ -38,65 +49,129 @@ if sys.implementation.name == "micropython":
             path = "/apps/" + a
     ASSET_PATH = path + "/assets/"
 
+def set_colour(c):
+    global colour
+    global last_change
+    global last_colour
+
+    if c == colour:
+        return
+
+    last_change = getT()
+    last_colour = colour
+    colour = c
+
+def set_leds(t):
+    global colour
+    global last_change
+    global last_colour
+    global intensity
+
+    unit = 2 * 3.14159 / LEDS
+
+    this_colour = colour
+
+    # Smooth fade between colour changes
+    transition = (getT() - last_change) * 10
+    if transition < 1:
+        transition_new = 1 - transition
+        
+        this_colour = (
+            int(last_colour[0] * transition_new + colour[0] * transition),
+            int(last_colour[1] * transition_new + colour[1] * transition),
+            int(last_colour[2] * transition_new + colour[2] * transition)
+        )
+
+    for i in range(LEDS):
+        rad = i * unit
+
+        rad_t = t * 2 * 3.14159
+
+        base_intensity = math.cos(rad + rad_t) ** 8
+
+        base_intensity = base_intensity * intensity
+
+        actual_colour = (int(this_colour[0] * base_intensity),
+                         int(this_colour[1] * base_intensity),
+                         int(this_colour[2] * base_intensity)
+        )
+
+
+        tildagonos.leds[i + 1] = actual_colour
+
+    tildagonos.leds.write()
+
+def up_intensity():
+    global intensity
+
+    if intensity == 0:
+        intensity = 0.05
+
+    intensity = min(1, intensity * 1.25)
+
+def down_intensity():
+    global intensity
+
+    intensity = max(0.05, intensity / 1.25)
+
 class Slide:
-    def draw(self, ctx):
+    def draw(self, ctx, t):
         raise "Not implemented"
 
 class Fleur(Slide):
-    def draw(self, ctx):
+    def draw(self, ctx, t):
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
-        ctx.image(ASSET_PATH + "fleur.jpg", -90, -80, 180, 164)
+        ctx.image(ASSET_PATH + "fleur.jpg", -100, -91, 200, 182)
 
-        for i in range(LEDS):
-            tildagonos.leds[i + 1] = SCOUTS_PURPLE
-        
-        tildagonos.leds.write()
-
+        set_colour(SCOUTS_PURPLE)
+        set_leds(t)
 
 class SE(Slide):
-    def draw(self, ctx):
+    def draw(self, ctx, t):
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
         ctx.image(ASSET_PATH + "se.jpg", -99, -100, 198, 200)
 
-        for i in range(LEDS):
-            tildagonos.leds[i + 1] = SE_DARK_GREEN
-        
-        tildagonos.leds.write()
+        set_colour(SE_DARK_GREEN)
+        set_leds(t)
         
 class Cake(Slide):
-    def draw(self, ctx):
+    def draw(self, ctx, t):
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
         ctx.image(ASSET_PATH + "cake.jpg", -80, -80, 160, 160)
 
-        for i in range(LEDS):
-            tildagonos.leds[i + 1] = SCOUTS_ORANGE
-        
-        tildagonos.leds.write()
-
-
+        set_colour(SCOUTS_ORANGE)
+        set_leds(t)
 
 class SlideApp(app.App):
     def __init__(self):
         eventbus.emit(PatternDisable())
         self.slides = [Fleur(), SE(), Cake()]
-        self.image_index = 0
-        self.wait = 4
         self.last_change = time.time()
         self.button_states = Buttons(self)
 
     def update(self, delta):
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
             self.minimise()
+            eventbus.emit(PatternEnable())
+            self.button_states.clear()
+        
+        if self.button_states.get(BUTTON_TYPES["DOWN"]):
+            down_intensity()
             self.button_states.clear()
 
-        if self.last_change + self.wait < time.time():
-            self.image_index = (self.image_index + 1) % len(self.slides)
-            self.last_change = time.time()
+        if self.button_states.get(BUTTON_TYPES["UP"]):
+            up_intensity()
+            self.button_states.clear()
 
     def draw(self, ctx):
         clear_background(ctx)
         ctx.save()
-        self.slides[self.image_index].draw(ctx)
+
+        t = getT()
+
+        slide_index = int(t) % len(self.slides)
+        self.slides[slide_index].draw(ctx, t)
+
         ctx.restore()
 
 __app_export__ = SlideApp
